a59177eca77318bd4c3882b2b4b523bc81a8bdf0
Chaz <chaz6cl@live.com>
2021-04-24 10:00:22 +1000

    Fixed other website domain

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -9,7 +9,7 @@ export default function MainHeader( {name, jobtitle, company, icon1, icon2, icon
             <div className="Social-Icons">
               <span className="Social-Icon"><a href="https://github.com/cazdev"><div className={["icon", "hoverAnim", "inAnim"].join(' ')}>{ icon1 }</div></a></span>
               <span className="Social-Icon"><a href="https://linkedin.com/in/chaz-lambrechtsen"><div className={["icon", "hoverAnim", "inAnim"].join(' ')}>{ icon2 }</div></a></span>
-              <span className="Social-Icon"><a href="https://chaz.page.com"><div className={["icon", "hoverAnim", "inAnim"].join(' ')}>{ icon3 }</div></a></span>
+              <span className="Social-Icon"><a href="https://chaz.page"><div className={["icon", "hoverAnim", "inAnim"].join(' ')}>{ icon3 }</div></a></span>
             </div>
         </div>
     )
